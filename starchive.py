3295d5a6946e6377aa68e7245267d7b3bb558169
8-bear <git@kalebhebert.email>
2020-01-09 13:10:05 -0800

    add switch statement to handle functions

--- starchive.py
+++ starchive.py
@@ -7,7 +7,25 @@ See README.md for usage instructions
 
 Started this repo 60% because of the usefulness and 40% because of the name.
 '''
-print(text2art("Starchive",font='starwars',chr_ignore=True) + "\n\n\n")
+
+# Globals
+
+def new_archive():
+    print("new archive")
+    return 1
+
+def existing_archive():
+    print("Existing Archive")
+    return 1
+
+def func_switch(arg):
+    switch = {
+        0: new_archive,
+        1: existing_archive,
+    }
+    ch_func = switch.get(arg, lambda: "Invalid Option")
+    print (ch_func())
+    return 1
 
 def handle_input(valid_arr, qstn):
     inp = -9669230
@@ -25,11 +43,10 @@ def handle_input(valid_arr, qstn):
     return inp
 
 def main():
+    print(text2art("Starchive",font='starwars',chr_ignore=True) + "\n\n\n")
     init_q = "Would you like to start a new archive [0] or update an existing archive? [1]\n"
 
-    choice = handle_input([0, 1], init_q)
-
+    ch = handle_input([0, 1], init_q)
+    func_switch(ch)
     
-
-
 main()
