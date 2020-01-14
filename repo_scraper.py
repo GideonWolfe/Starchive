d5ac5229fcaff90f26e42fdd2c35b499a26ec20b
8-bear <git@kalebhebert.email>
2020-01-13 19:21:12 -0800

    add example directory for tracking starred repos

---
+++ repo_scraper.py
@@ -0,0 +1,24 @@
+from github import Github
+from gitlab import Gitlab
+
+
+# user: username on gitlab/hub | token: personal access token
+
+
+def get_github_repos(user,token):
+    gh = Github(token)
+    repos = []
+
+    for repo in gh.get_user().get_starred():
+        repos.append(repo.clone_url)
+
+    return repos
+
+def get_gitlab_repos(user,token):
+    gl = Gitlab('https://gitlab.com', private_token=token)
+    repos = []
+
+    for repo in gl.projects.list(starred=True):
+        print(repo.web_url)
+    
+    return repos
