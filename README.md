5124f2947bfb1f8e56798bcb5133f33496780e4d
8-bear <git@kalebhebert.email>
2020-01-13 19:42:23 -0800

    cleanup unncessary code, update README, add default yaml config file

--- README.md
+++ README.md
@@ -6,5 +6,9 @@
 - python3
     - PyGithub
     - python-gitlab
+    - PyYAML
+    - text2art
+
+##### Usage
 
 
--- starchive.py
+++ starchive.py
@@ -11,46 +11,6 @@ See README.md for usage instructions
 Started this repo 60% because of the usefulness and 40% because of the name.
 '''
 
-# Globals
-
-# Loads config file
-def load_config(config_file):
-    with open(config_file, 'r') as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-
-# Authenticates and loads github profile for API access
-def load_github(ghuser, ghpass, ghtoken):
-    g = Github(ghuser, ghpass, ghtoken)
-
-    for repo in g.get_user().get_repos():
-        print(repo.name)
-
-
-# Function to be executed if user wants to archive a new user
-def new_archive():
-    print("New archive")
-    userUrl = input("Enter the URL of the git user you wish to Starchive")
-    return 1
-
-# Function to be executed when a user wants to update existing archives
-def existing_archive():
-    print("Existing Archive")
-    return 1
-
-
-# Maps users choice to the correct function
-def func_switch(arg):
-    switch = {
-        0: new_archive,
-        1: existing_archive,
-    }
-    ch_func = switch.get(arg, lambda: "Invalid Option")
-    print (ch_func())
-    return 1
-
 # Makes sure the user chose a valid action 
 def handle_input(valid_arr, qstn):
     inp = -9669230
@@ -67,6 +27,35 @@ def handle_input(valid_arr, qstn):
     
     return inp
 
+# Loads config file
+def load_config(config_file):
+    with open(config_file, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+# Executed if user wants to archive a new user
+def new_archive():
+    print("New archive")
+    userUrl = input("Enter the URL of the git user you wish to Starchive")
+    return 1
+
+# Executed when a user wants to update existing archives
+def existing_archive():
+    print("Existing Archive")
+    return 1
+
+# Maps users choice to the correct function
+def func_switch(arg):
+    switch = {
+        0: new_archive,
+        1: existing_archive,
+    }
+    ch_func = switch.get(arg, lambda: "Invalid Option")
+    print (ch_func())
+    return 1
+
 def main():
     # Show Program Title
     print(text2art("Starchive",font='starwars',chr_ignore=True) + "\n\n\n")
@@ -75,17 +64,18 @@ def main():
     config = load_config('config.yml')
     outputdir = config['outputdir'][0] # Folder where starchives will be stored
     ghuser = config['githubuser'][0]
-    ghpass = config['githubpassword'][0]
+    gluser = config['gitlabuser'][0]
     ghtoken = config['githubtoken'][0]
-    load_github(ghuser, ghpass, ghtoken)
+    gltoken = config['gitlabtoken'][0]
 
     # Make the output filepath if it doesn't already exist
-    Path(outputdir).mkdir(parents=True, exist_ok=True)
+    if(!Path(outputdir).is_dir()):
+        Path(outputdir).mkdir(parents=True, exist_ok=True)
 
     # Prompt user for desired action
     init_q = "Would you like to: \nstart a new archive [0] \n update an existing archive? [1]\n"
 
-    # Records user action choice
+    # Handle user action choice
     ch = handle_input([0, 1], init_q)
     func_switch(ch)
     
